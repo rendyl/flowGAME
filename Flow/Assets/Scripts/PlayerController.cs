fbc6fe1df325f13dad6603d35ecb05402b2170c7
Rendy Latbi <rlatbi@univ-lyon2.fr>
2019-11-06 10:51:36 +0100

    SceneSample2 is miiiiiiiine

---
+++ Flow/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        speed = 0;
+        Debug.Log("dead");
+        Application.Quit();
+    }
+}
