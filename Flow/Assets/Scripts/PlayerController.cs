57f42f9cdb4dd93e3ce9108c3d935dfb3c1f075d
Rendy Latbi <rlatbi@univ-lyon2.fr>
2019-11-06 13:58:00 +0100

    Q & D

--- Flow/Assets/Scripts/PlayerController.cs
+++ Flow/Assets/Scripts/PlayerController.cs
@@ -5,23 +5,45 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    private bool wallRunning;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        wallRunning = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (transform.position.x > -0.5) transform.position -= Vector3.right;
+            else
+            {
+                wallRunning = true;
+                transform.localScale = new Vector3(transform.localScale.x, 0.5f * transform.localScale.y, transform.localScale.z);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (transform.position.x < 0.5) transform.position += Vector3.right;
+            else
+            {
+                wallRunning = true;
+                transform.localScale = new Vector3(transform.localScale.x, 0.5f * transform.localScale.y, transform.localScale.z);
+            }
+        }
         transform.position += Vector3.forward * speed * Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        speed = 0;
-        Debug.Log("dead");
-        Application.Quit();
+        if (other.gameObject.gameObject.CompareTag("Obstacle"))
+        {
+            speed = 0;
+            Debug.Log("dead");
+            Application.Quit();
+        }
     }
 }
