bb149b13e1e7617f7cc4ee5b6f9ed23448b5544d
aurel <aurambroise@gmail.com>
2019-11-27 17:07:42 +0100

    mur destroyer d'objet a fix :(

--- Flow/Assets/DeleteObject.cs
+++ Flow/Assets/DeleteObject.cs
@@ -4,25 +4,9 @@ using UnityEngine;
 
 public class DeleteObject : MonoBehaviour
 {
-
-    public GameObject player;
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter(Collision col)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //transform.position = player.transform.position - player.transform.forward * -5;
-        //transform.position += Vector3.forward * player.GetComponent<PlayerController>().speed * Time.deltaTime;
-    }
-
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        //Debug.Log(other.tag);
-        Destroy(other.gameObject);
+        Debug.Log(col.gameObject.name);
+        Destroy(col.gameObject);   
     }
 }
