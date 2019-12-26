36e562cd3cd0868e4cd4862bfe8ef71ffd479b41
Lupusanghrenn <brisingr34130@gmail.com>
2019-12-26 20:14:28 +0100

    Amelioration du random et rajout d obs

--- Flow/Assets/Scripts/GenEndless.cs
+++ Flow/Assets/Scripts/GenEndless.cs
@@ -24,6 +24,7 @@ public class GenEndless : MonoBehaviour
     {
         currentPoint = startPoint;
         targetTime = 0;
+        Debug.Log(easyToMedium);
         mediumToHard += easyToMedium;
         hardToUltra += mediumToHard;
     }
@@ -49,19 +50,19 @@ public class GenEndless : MonoBehaviour
         //Destroy(Instantiate(Resources.Load<GameObject>("Prefabs/Obs0"), currentPoint, Quaternion.identity),6);
 
         currentPoint += new Vector3(0, 0, 5f);
-        int index = Random.Range(1, 23);
+        int index = 0;
         // tirage du Obs aléatoirement
         int proba = Random.Range(1, 4);
         // tirage de la probabilite 1 chance sur 3
 
-        int easy = Random.Range(1, 5);
-        int medium = Random.Range(6, 10);
-        int hard = Random.Range(11, 14);
-        int ultra = Random.Range(15, 23);
+        int easy = Random.Range(1, 6);
+        int medium = Random.Range(6, 12);
+        int hard = Random.Range(12, 20);
+        int ultra = Random.Range(20, 26);
 
-        while ((lastIndex == 18 && ultra == 19) || (lastIndex == 19 && ultra == 18))
+        while ((lastIndex == 20 && ultra == 21) || (lastIndex == 21 && ultra == 20))
         {
-            ultra = Random.Range(15, 23);
+            ultra = Random.Range(20, 26);
         }
 
         if (targetTime >= easyToMedium)
